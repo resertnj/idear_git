526dfa5867c3f37ca98f745a3cb4ab515584eb4c
root <root>
2022-10-01 15:29:54 +0800

    branch1

--- src/main/java/com/powernode/githup/git.java
+++ src/main/java/com/powernode/githup/git.java
@@ -8,5 +8,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("111111111111111111");
+        System.out.println("22222");
     }
 }
