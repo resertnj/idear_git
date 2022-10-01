9a493ecb7723f632886c2a787ed853d60ef0080b
root <root>
2022-10-01 08:59:44 +0800

    first

---
+++ src/main/java/com/powernode/githup/git.java
@@ -0,0 +1,11 @@
+package com.powernode.githup;
+
+/**
+ * @creat by mike
+ * @time 2022/10/1 8:56 周六
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
